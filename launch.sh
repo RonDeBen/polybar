bde59dceb5c729c6e95473fe2eedc3b34f2f145b
Ron DeBenedetti <ron.debenedetti@praeses.com>
2023-12-30 22:37:03 -0600

    pluck custom forest theme out of polybar-themes

--- launch.sh
+++ launch.sh
@@ -1,81 +1,39 @@
 #!/usr/bin/env bash
 
-dir="$HOME/.config/polybar"
-themes=(`ls --hide="launch.sh" $dir`)
+# Add this script to your wm startup file.
 
+DIR="$HOME/.config/polybar"
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Define your monitors
+primary_monitor="DP-2-2"
+laptop_monitor="eDP-1"
+secondary_monitor="DP-2-3"
+
+# Check if monitors are connected
+primary_connected=$(xrandr --query | grep "$primary_monitor connected")
+laptop_connected=$(xrandr --query | grep "$laptop_monitor connected")
+secondary_connected=$(xrandr --query | grep "$secondary_monitor connected")
+
+# Function to launch bar
 launch_bar() {
-	# Terminate already running bar instances
-	killall -q polybar
-
-	# Wait until the processes have been shut down
-	while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
-
-	# Launch the bar
-	if [[ "$style" == "hack" || "$style" == "cuts" ]]; then
-		polybar -q top -c "$dir/$style/config.ini" &
-		polybar -q bottom -c "$dir/$style/config.ini" &
-	elif [[ "$style" == "pwidgets" ]]; then
-		bash "$dir"/pwidgets/launch.sh --main
-	else
-		polybar -q main -c "$dir/$style/config.ini" &	
-	fi
+  MONITOR=$1 polybar -q $2 -c "$DIR"/config.ini &
 }
 
-if [[ "$1" == "--material" ]]; then
-	style="material"
-	launch_bar
+# Launch Polybar based on monitor connection
+if [ "$primary_connected" ]; then
+    launch_bar $primary_monitor main
+fi
 
-elif [[ "$1" == "--shades" ]]; then
-	style="shades"
-	launch_bar
+if [ "$laptop_connected" ]; then
+    launch_bar $laptop_monitor secondary
+fi
 
-elif [[ "$1" == "--hack" ]]; then
-	style="hack"
-	launch_bar
-
-elif [[ "$1" == "--docky" ]]; then
-	style="docky"
-	launch_bar
-
-elif [[ "$1" == "--cuts" ]]; then
-	style="cuts"
-	launch_bar
-
-elif [[ "$1" == "--shapes" ]]; then
-	style="shapes"
-	launch_bar
-
-elif [[ "$1" == "--grayblocks" ]]; then
-	style="grayblocks"
-	launch_bar
-
-elif [[ "$1" == "--blocks" ]]; then
-	style="blocks"
-	launch_bar
-
-elif [[ "$1" == "--colorblocks" ]]; then
-	style="colorblocks"
-	launch_bar
-
-elif [[ "$1" == "--forest" ]]; then
-	style="forest"
-	launch_bar
-
-elif [[ "$1" == "--pwidgets" ]]; then
-	style="pwidgets"
-	launch_bar
-
-elif [[ "$1" == "--panels" ]]; then
-	style="panels"
-	launch_bar
-
-else
-	cat <<- EOF
-	Usage : launch.sh --theme
-		
-	Available Themes :
-	--blocks    --colorblocks    --cuts      --docky
-	--forest    --grayblocks     --hack      --material
-	--panels    --pwidgets       --shades    --shapes
-	EOF
+if [ "$secondary_connected" ]; then
+    launch_bar $secondary_monitor vert
 fi
